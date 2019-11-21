09ec84de85cec6f7720714899c73796f19c66ce1
Prochot <prochot@gmail.com>
2019-11-21 15:54:42 -0800

    Original working procedure

---
+++ USGS Water Data/USGS Water Data.cs
@@ -0,0 +1,41 @@
+using System.IO;
+using System.Data;
+using System.Net;
+using System.Xml;
+using System.Text;
+using Microsoft.SqlServer.Server;
+using System.Data.SqlTypes;
+using System.Security.Cryptography.X509Certificates;
+using System.Net.Security;
+
+public partial class StoredProcedures
+{
+    [Microsoft.SqlServer.Server.SqlProcedure]
+    public static void UspGetUSGSDataBySite(SqlInt32 site)
+    {
+        HttpWebRequest request =
+            (HttpWebRequest)WebRequest.Create("https://waterservices.usgs.gov/nwis/iv/?format=waterml,2.0&indent=on&sites=" + site + "&siteStatus=all");
+
+        request.Method = "GET";
+        request.ContentLength = 0;
+        request.ContentType = "application/xml";
+        request.Accept = "application/xml";
+
+        ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+        using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+        {
+            using (Stream receiveStream = response.GetResponseStream())
+            {
+                using (StreamReader readStream = new StreamReader(receiveStream, Encoding.UTF8))
+                {
+                    string strContent = readStream.ReadToEnd();
+                    XmlDocument xdoc = new XmlDocument();
+                    xdoc.LoadXml(strContent);
+                    SqlDataRecord record = new SqlDataRecord(new SqlMetaData("USGS_XML", SqlDbType.Xml));
+                    record.SetSqlXml(0, new SqlXml(new XmlNodeReader(xdoc)));
+                    SqlContext.Pipe.Send(record);
+                }
+            }
+        }
+    }
+}
